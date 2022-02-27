e9da460a6b20d53f1144511f484c5da943e8d56b
liuqiang <960504193@qq.com>
2022-02-27 17:22:20 +0800

    1

---
+++ readme.md
@@ -0,0 +1,4 @@
+庆祝离职，搞一个spring-cloud玩玩
+
+spring-boot 2.6.4
+
---
+++ src/main/java/com/fun/SpringCloudFunApplication.java
@@ -0,0 +1,13 @@
+package com.fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFunApplication.class, args);
+    }
+
+}
